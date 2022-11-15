a01542b44f3512518bacfd360344928518418b2e
gauravrathod007 <gauravrathod11@gmail.com>
2022-11-15 09:00:11 +0530

    secondec2

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,10 @@ resource "aws_instance" "ec3" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
 }
+
+
+resource "aws_instance" "ec3" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.meduim"
+}
+
